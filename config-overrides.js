b760a7a4035bba9cf9fa104ad16e2e93877f881c
Beatriz <beatriz.diniz@sp.senai.br>
2019-05-23 14:33:25 -0300

    Refactor antd theme setup

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const lessToJs = require('less-vars-to-js');
 const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+
+const themePath = './src/assets/styles/antd-custom-variables.less';
+const themeFile = fs.readFileSync(path.join(__dirname, themePath), 'utf8');
+const themeVariables = lessToJs(themeFile);
 
 module.exports = override(
   fixBabelImports("import", {
@@ -8,6 +15,6 @@ module.exports = override(
   }),
   addLessLoader({
     javascriptEnabled: true,
-    modifyVars: { '@primary-color': '#1DA57A' },
+    modifyVars: themeVariables,
   }),
 );
