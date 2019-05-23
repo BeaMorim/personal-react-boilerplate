255364f9c538a01d79af0cc12510b5acc9ac94ee
Beatriz <beatriz.diniz@sp.senai.br>
2019-05-23 10:49:28 -0300

    Set enzyme config

--- src/page.js
+++ src/page.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const Page = () => {
   return (
-    <h1>Page</h1>
+    <div>
+      <span className="some-class" />
+    </div>
   );
 };
 
