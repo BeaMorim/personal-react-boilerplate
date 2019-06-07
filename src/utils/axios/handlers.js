cfa9a11a113b515563e3045d0d428f39271a8578
Beatriz <beatriz.diniz@sp.senai.br>
2019-06-07 13:16:04 -0300

    Documenting request handlers

--- src/utils/axios/handlers.js
+++ src/utils/axios/handlers.js
@@ -1,3 +1,7 @@
+/**
+ * Handler that runs before request is done
+ * @param {Object} resquest Request object
+ */
 const requestHandler = async resquest => {
   if (localStorage.getItem("authorization")) {
     resquest.headers = {
@@ -8,10 +12,23 @@ const requestHandler = async resquest => {
   return resquest;
 };
 
+/**
+ * Handler that runs before request is done and if some error occurs
+ * @param {Object} error Request error object
+ */
 const requestErrorHandler = error => Promise.reject(error);
 
+
+/**
+ * Handler that runs when response is received
+ * @param {Object} response API response
+ */
 const responseHandler = async response => response;
 
+/**
+ * Handler that runs when an error is returned from API
+ * @param {Object} error API response error
+ */
 const responseErrorHandler = error => {
   // Default error handlers
   return Promise.reject(error);
